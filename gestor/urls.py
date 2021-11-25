c9ab95a49610c652f1729bedc922eec1bf08af7b
DavidPerezVelilla <dvdpv926@gmail.com>
2021-11-25 20:31:14 +0100

    8 commit

--- gestor/urls.py
+++ gestor/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
    path('buscarequipos/', SearchResultsListView.as_view(), name="buscar" ),
    path('equipo_list', EquiposListView.as_view(), name='listado'),
    path('detalle_equipo/<int:pk>', EquipoDetailView.as_view(), name="detalle_equipo"),
+   path('detalle_jugador/<int:pk>', JugadoresDetailView.as_view(), name="detalle_jugador"),
    path('equipo/crear', crear_equipo, name='crear_equipo'),
 ]
